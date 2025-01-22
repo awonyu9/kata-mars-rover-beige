506eacdfbc0f2f3d4a7048c31b04804daa587ad0
Hugo PANEL <hugopanel@gmail.com>
2025-01-22 15:26:56 +0100

    Fix test function name

--- python/test_rover.py
+++ python/test_rover.py
@@ -2,15 +2,15 @@ from rover import Rover
 import pytest
 
 
-test_data = [
+test_data_movement = [
     ("north", 4, 2, 4, 3),
     ("west", 4, 2, 3, 2),
     ("south", 4, 2, 4, 1),
     ("east", 4, 2, 5, 2)
 ]
 
-@pytest.mark.parametrize("direction, x, y, expected_x, expected_y", test_data)
-def test_move_forward_north(direction, x, y, expected_x, expected_y):
+@pytest.mark.parametrize("direction, x, y, expected_x, expected_y", test_data_movement)
+def test_move_forward(direction, x, y, expected_x, expected_y):
     rover = Rover(x, y, direction)
     rover.move_forward()
 
